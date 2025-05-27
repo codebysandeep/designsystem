3b2f71c5d360be903d4ef47b927bb4a8f9d961b5
Sandeep Upadhyay <san_allways@yahoo.com>
2025-05-27 15:58:05 -0400

    feat: added theme filter script

--- packages/tokens/src/config/filters.js
+++ packages/tokens/src/config/filters.js
@@ -5,3 +5,32 @@
  * @author Sandeep Upadhyay
  */
 
+/**
+ * Filters tokens based on the provided attributes.
+ * @param {Object} categoryConfig - attribute with category.
+ * @returns {function} - A function that filters tokens based on the provided attributes.
+ */
+export function createFilter(categoryConfig) {
+  return (token) => {
+    if (!categoryConfig) return true;
+    
+    if (categoryConfig.attributes && token.attributes) {
+        return Object.keys(categoryConfig.attributes).every((key) => token.attributes[key] === categoryConfig.attributes[key]);
+    }
+    return false;
+  };
+}
+
+/**
+ * Filters tokens based on the provided themes.
+ * @param {string[]} themeType - Theme types used to filter tokens.
+ * @returns {function} - A function that filters tokens based on the provided theme types.
+ */
+export function createThemeFilter(themeType) {
+  return (token) =>  {
+    if (!token || typeof token !== 'object') return false;
+    if (token.type === themeType) return true;
+    if (token.value?.type === themeType) return true;
+    return token.path?.includes(themeType) ?? false;
+  }
+}
